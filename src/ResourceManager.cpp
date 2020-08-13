05f1aad89a1551dc98c109bc12493b6d332c24b6
jgesc <37676259+jgesc@users.noreply.github.com>
2020-08-13 20:54:38 +0200

    Code cleaning

--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 ifstream ResourceManager::getResourceStream(string path)
 {
-  ifstream stream = ifstream(path);
+  ifstream stream = ifstream("./Resources/" + path);
   if(stream.fail()) {
-    // TODO: File does not exist code here
+    // TODO: File does not exist
     throw 0;
   }
   return stream;
